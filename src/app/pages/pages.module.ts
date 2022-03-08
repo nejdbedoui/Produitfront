fc337b90b984e0846c62ba452006514aec8ea1e4
nejdbedoui <74482153+nejdbedoui@users.noreply.github.com>
2022-03-08 23:04:09 +0100

    empty ngxadmin

---
+++ src/app/pages/pages.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { NbMenuModule } from '@nebular/theme';
+
+import { ThemeModule } from '../@theme/theme.module';
+import { PagesComponent } from './pages.component';
+import { ECommerceModule } from './e-commerce/e-commerce.module';
+import { PagesRoutingModule } from './pages-routing.module';
+import { DatePipe } from '@angular/common';
+
+
+
+@NgModule({
+  imports: [
+    PagesRoutingModule,
+    ThemeModule,
+    NbMenuModule,
+    ECommerceModule,
+  ],
+  declarations: [
+    PagesComponent,
+  ],
+  providers: [
+    DatePipe
+  ],
+})
+export class PagesModule {
+}
